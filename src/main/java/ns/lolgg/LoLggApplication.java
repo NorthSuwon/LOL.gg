c16c2a90ece269f497e49c4f5e45418999f77ff3
Ta-Ye <cty1272@naver.com>
2021-04-24 13:40:04 +0900

    개발환경 구성

---
+++ src/main/java/ns/lolgg/LoLggApplication.java
@@ -0,0 +1,13 @@
+package ns.lolgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ns/lolgg/LoLggApplicationTests.java
@@ -0,0 +1,13 @@
+package ns.lolgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoLggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
