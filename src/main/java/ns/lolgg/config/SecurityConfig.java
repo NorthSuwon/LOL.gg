d08b976073911a6cbd3db133cc1fde4d047368aa
Ta-Ye <cty1272@naver.com>
2021-04-26 13:52:03 +0900

    JSP에서 thymeleaf로 변경, Spring Security 적용

--- src/main/java/ns/lolgg/config/SecurityConfig.java
+++ src/main/java/ns/lolgg/config/SecurityConfig.java
@@ -1,67 +1,39 @@
-//package ns.lolgg.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//import ns.lolgg.service.UserService;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests()
-//			//접근 허용
-//			.antMatchers("/css/**", "/js/**", "/images/**", "/resources/**", "/h2-console/**", "/webjars/**", "/signup", "/index", "/")
-//			.permitAll()
-//			.anyRequest().authenticated()
-//			.and()
-//			//iframe 허용
-//			.headers()
-//			.frameOptions().disable()
-//			.and()
-//			//DB 어드민 접속 허용
-//			.csrf()
-//			.ignoringAntMatchers("/h2-console/**")
-//			.and()
-//			//로그인
-//			.formLogin()
-//			.loginPage("/login")
-//			.loginProcessingUrl("/signin")
-//			.defaultSuccessUrl("index")
-//			.failureUrl("/index")
-//			.usernameParameter("userId")
-//			.passwordParameter("password")
-//			.permitAll();
-//			.and()
-//		
-//		
-//		
-//	}
-//	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(null)
-//	}
-//	
-//	@Bean
-//	public BCryptPasswordEncoder BCryptPasswordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//	
-//	@Bean
-//	public UserService userService() throws Exception {
-//		return new UserService();
-//	}
-//	
-//	
-//}
+package ns.lolgg.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    	http
+                .authorizeRequests(request->
+                        request.antMatchers("/", "/login", "/signin").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(
+                        login->login.loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/login-error")
+                )
+                .logout(logout->logout.logoutSuccessUrl("/"))
+                .exceptionHandling(e->e.accessDeniedPage("/access-denied"))
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                ;
+    }
+}
--- src/main/java/ns/lolgg/controller/UserController.java
+++ src/main/java/ns/lolgg/controller/UserController.java
@@ -1,47 +1,22 @@
 package ns.lolgg.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
-import ns.lolgg.domain.User;
-import ns.lolgg.dto.UserDTO.UserLogin;
 import ns.lolgg.dto.UserDTO.UserRegi;
 import ns.lolgg.service.UserService;
 
-@SessionAttributes("user")
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@PostMapping("/logins")
-	public String login(UserLogin userLogin, HttpSession session) {
-		User user = userService.findUserById(userLogin.getId());
-		if (user == null || !userService.checkUserPassword(user, userLogin.getPassword())) {
-			return "redirect:login";
-		}
-
-		session.setAttribute("user", user);
-		return "index";
-	}
-	
 	@GetMapping("/login")
 	public String loginpage() {
 		return "login";
-	}
-	
-
-	@GetMapping("/logout")
-	public String logout(SessionStatus status) {
-		status.setComplete();
-		return "redirect:/";
 	}
 
 	@PostMapping("/signin")
